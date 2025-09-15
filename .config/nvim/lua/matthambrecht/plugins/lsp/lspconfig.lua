dea28f8aca28401327ae192b257462505fff0b70
Matthew Hambrecht <hambrechtmatt@gmail.com>
2025-09-14 22:34:28 -0400

    Nvim config updates

--- .config/nvim/lua/matthambrecht/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/matthambrecht/plugins/lsp/lspconfig.lua
@@ -9,8 +9,8 @@ return {
   config = function()
     -- import cmp-nvim-lsp plugin
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local keymap = vim.keymap
 
-    local keymap = vim.keymap -- for conciseness
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(ev)
@@ -34,26 +34,26 @@ return {
         opts.desc = "Show LSP type definitions"
         keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
+        opts.desc = "Show documentation for what is under cursor"
+        keymap.set("n", "gK", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
         opts.desc = "See available code actions"
-        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+        keymap.set({ "n", "v" }, "gca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
         opts.desc = "Smart rename"
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+        keymap.set("n", "gr", vim.lsp.buf.rename, opts) -- smart rename
 
         opts.desc = "Show buffer diagnostics"
-        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+        keymap.set("n", "\\gD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
         opts.desc = "Show line diagnostics"
-        keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+        keymap.set("n", "\\gd", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
         opts.desc = "Go to previous diagnostic"
         keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
         opts.desc = "Go to next diagnostic"
         keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-        opts.desc = "Show documentation for what is under cursor"
-        keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
         opts.desc = "Restart LSP"
         keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
@@ -77,6 +77,8 @@ return {
     vim.lsp.config("*", {
       capabilities = capabilities,
     })
+
+    -- Web Development
 
     vim.lsp.config("svelte", {
       on_attach = function(client, bufnr)
@@ -102,6 +104,7 @@ return {
       filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
     })
 
+    -- Lua
     vim.lsp.config("lua_ls", {
       settings = {
         Lua = {
@@ -115,6 +118,35 @@ return {
         },
       },
     })
+
+    -- Python (Pyright)
+    vim.lsp.config("pyright", {
+      settings = {
+        python = {
+          analysis = {
+            typeCheckingMode = "standard", -- "off" | "basic" | "standard" | "strict"
+            autoImportCompletions = true,
+            diagnosticMode = "workspace",
+            useLibraryCodeForTypes = true,
+          },
+        },
+      },
+    })
+
+    -- Bash
+    vim.lsp.config("bashls", {})
+
+    -- Go
+    vim.lsp.config("gopls", {
+      settings = {
+        gopls = {
+          analyses = { unusedparams = true, unreachable = true, unusedvariable = true },
+          staticcheck = true,
+          gofumpt = true,
+        },
+      },
+    })
+
   end,
 }
 
