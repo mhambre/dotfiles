22313246864531955a289d963f54c76e7ec3245a
Matthew Hambrecht <matthew.hambrecht@patapsco.ai>
2026-05-28 20:09:50 -0400

    Add some extra aliases

--- .bashrc.d/tools.sh
+++ .bashrc.d/tools.sh
@@ -1,8 +1,13 @@
 # Shortcut for common tool use
+
+# Common enough aliases
 alias src="source ~/.bashrc"
 alias la="ls -la"
 alias lg="lazygit"
-function vzf () { # Open fzf file in vim
+alias k9="kill -9"
+
+# Open fzf file in vim
+function vzf () { 
     file=$(fzf)
     vi $file
 }
@@ -11,3 +16,18 @@ if command -v nvim >/dev/null 2>&1; then
 else
   alias vim='vim'
 fi
+
+# Find a port and kill the process using it
+function pok () {
+    if [ -z "$1" ]; then
+        echo "Usage: pf <port>"
+        return 1
+    fi
+    lsof -i :$1
+    if [ $? -eq 0 ]; then
+        echo "Killing process on port $1"
+        lsof -t -i :$1 | xargs kill -9
+    else
+        echo "No process found on port $1"
+    fi
+}
