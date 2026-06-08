adc791c2ea0d6ac85d7ea8b2f05cd8fc2e27faf0
Matthew <matthew.hambrecht@patapsco.ai>
2026-06-08 16:31:14 -0400

    relative line numbers on neotree

--- .config/nvim/lua/matthambrecht/plugins/tree.lua
+++ .config/nvim/lua/matthambrecht/plugins/tree.lua
@@ -73,6 +73,15 @@ return {
 			window = {
 				width = 32,
 			},
+			event_handlers = {
+				{
+					event = "neo_tree_buffer_enter",
+					handler = function()
+						vim.opt_local.number = true
+						vim.opt_local.relativenumber = true
+					end,
+				},
+			},
 		}
 	end,
 }
