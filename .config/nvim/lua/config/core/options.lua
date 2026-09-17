1e9a41749f8b25bf67fc5058136c8673f3e2ebd4
Matthew <matthew.hambrecht@patapsco.ai>
2026-09-17 14:19:42 -0400

    fix: Vim clipboard overwritten by yank

--- .config/nvim/lua/config/core/options.lua
+++ .config/nvim/lua/config/core/options.lua
@@ -17,7 +17,14 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 --- Clipboard
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = ""
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		if vim.v.event.operator == "y" then
+			vim.fn.setreg("+", vim.v.event.regcontents, vim.v.event.regtype)
+		end
+	end,
+})
 
 --- Editor Lines
 vim.opt.colorcolumn = "80,120"
